b904a60d03b4d4709955c07d5428deccd812021a
Techboy v1050 <tokkivanc13@gmail.com>
2023-07-29 16:36:17 +0300

    Change the loops to be able to exit them

--- Assets/Code/game.py
+++ Assets/Code/game.py
@@ -8,22 +8,19 @@ goal_x, goal_y = 2, 0
 
 main = engine.Main()
 
-window = main.window.new_window('Level 1', lvl_width * tile_width, lvl_height * tile_height)
+lvl_1 = main.window.new_window('Level 1', lvl_width * tile_width, lvl_height * tile_height)
 
-
-@main.window.loop
-def loop():
-    global player_x, player_y
-    if main.event.key_down(main.keys.KEY_w, window):
+while main.window.running(lvl_1):
+    if main.event.key_down(main.keys.KEY_w, lvl_1):
         player_y -= 1
 
-    if main.event.key_down(main.keys.KEY_a, window):
+    if main.event.key_down(main.keys.KEY_a, lvl_1):
         player_x -= 1
 
-    if main.event.key_down(main.keys.KEY_s, window):
+    if main.event.key_down(main.keys.KEY_s, lvl_1):
         player_y += 1
 
-    if main.event.key_down(main.keys.KEY_d, window):
+    if main.event.key_down(main.keys.KEY_d, lvl_1):
         player_x += 1
 
     if player_x < 0:
@@ -39,7 +36,7 @@ def loop():
         player_y = lvl_height - 1
 
     objects.draw_object(
-        window,
+        lvl_1,
         '../Images/player.png',
         player_x * tile_width,
         player_y * tile_height,
@@ -49,7 +46,7 @@ def loop():
     )
 
     objects.draw_object(
-        window,
+        lvl_1,
         '../Images/goal.png',
         goal_x * tile_width,
         goal_y * tile_height,
@@ -58,8 +55,9 @@ def loop():
         main
     )
 
-    collision.is_colliding(player_x, player_y, [(goal_x, goal_y)], lambda: main.quit(window))
-
+    collision.is_colliding(player_x, player_y, [(goal_x, goal_y)], lambda: main.quit(lvl_1))
+    if player_x == goal_x and player_y == goal_y:
+        break
 
 lvl_width, lvl_height = 5, 3
 tile_width, tile_height = 92, 92
@@ -71,7 +69,7 @@ walls = [(1, 1), (1, 2), (3, 0), (3, 1)]
 main = engine.Main()
 
 
-window = main.window.new_window('Level 1', lvl_width * tile_width, lvl_height * tile_height)
+lvl_2 = main.window.new_window('Level 2', lvl_width * tile_width, lvl_height * tile_height)
 
 
 def up():
@@ -94,22 +92,20 @@ def right():
     player_x += 1
 
 
-@main.window.loop
-def loop():
-    global player_x, player_y
-    if main.event.key_down(main.keys.KEY_w, window):
+while main.window.running(lvl_2):
+    if main.event.key_down(main.keys.KEY_w, lvl_2):
         player_y -= 1
         collision.is_colliding(player_x, player_y, walls, down)
 
-    if main.event.key_down(main.keys.KEY_a, window):
+    if main.event.key_down(main.keys.KEY_a, lvl_2):
         player_x -= 1
         collision.is_colliding(player_x, player_y, walls, right)
 
-    if main.event.key_down(main.keys.KEY_s, window):
+    if main.event.key_down(main.keys.KEY_s, lvl_2):
         player_y += 1
         collision.is_colliding(player_x, player_y, walls, up)
 
-    if main.event.key_down(main.keys.KEY_d, window):
+    if main.event.key_down(main.keys.KEY_d, lvl_2):
         player_x += 1
         collision.is_colliding(player_x, player_y, walls, left)
 
@@ -126,7 +122,7 @@ def loop():
         player_y = lvl_height - 1
 
     objects.draw_object(
-        window,
+        lvl_2,
         '../Images/player.png',
         player_x * tile_width,
         player_y * tile_height,
@@ -136,7 +132,7 @@ def loop():
     )
 
     objects.draw_object(
-        window,
+        lvl_2,
         '../Images/goal.png',
         goal_x * tile_width,
         goal_y * tile_height,
@@ -147,7 +143,7 @@ def loop():
 
     for wall in walls:
         objects.draw_object(
-            window,
+            lvl_2,
             '../Images/wall.png',
             wall[0] * tile_width,
             wall[1] * tile_height,
@@ -156,4 +152,6 @@ def loop():
             main
         )
 
-    collision.is_colliding(player_x, player_y, [(goal_x, goal_y)], lambda: main.quit(window))
+    collision.is_colliding(player_x, player_y, [(goal_x, goal_y)], lambda: main.quit(lvl_1))
+    if player_x == goal_x and player_y == goal_y:
+        break
