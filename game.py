4d7cce57863e56128a8432b88c43ed560ed6abd2
OPItU <tokkivanc13@gmail.com>
2023-09-30 22:51:34 +0300

    Added a new level (I am writing this at 22:50 30.09.2023 GMT+03:00 help)

--- game.py
+++ game.py
@@ -4,96 +4,6 @@ from Assets.Code import objects
 
 count = 0
 
-lvl_width, lvl_height = 3, 1
-tile_width, tile_height = 128, 128
-player_x, player_y = 0, 0
-goal_x, goal_y = 2, 0
-
-tb, to = False, False  # whether blue and orange are triggered
-blue_t, orange_t = [], []  # blue and orange triggers
-blue_w, orange_w = [], []  # blue and orange walls
-
-main = engine.Main()
-
-lvl_1 = main.window.new_window('Level 1', lvl_width * tile_width, lvl_height * tile_height)
-main.window.change_icon('Assets/Images/player.png')
-
-while main.window.running(lvl_1):
-    main.draw.draw_rect(lvl_1, 0, 0, lvl_width * tile_width, lvl_height * tile_height, (0, 0, 0, 255))
-    if main.event.key_down(main.keys.KEY_w):
-        count += 1
-        if count == 1:
-            player_y -= 1
-
-    elif main.event.key_down(main.keys.KEY_a):
-        count += 1
-        if count == 1:
-            player_x -= 1
-
-    elif main.event.key_down(main.keys.KEY_s):
-        count += 1
-        if count == 1:
-            player_y += 1
-
-    elif main.event.key_down(main.keys.KEY_d):
-        count += 1
-        if count == 1:
-            player_x += 1
-
-    else:
-        count = 0
-
-    if player_x < 0:
-        player_x = 0
-
-    if player_x >= lvl_width:
-        player_x = lvl_width - 1
-
-    if player_y < 0:
-        player_y = 0
-
-    if player_y >= lvl_height:
-        player_y = lvl_height - 1
-
-    objects.draw_object(
-        lvl_1,
-        'Assets/Images/player.png',
-        player_x * tile_width,
-        player_y * tile_height,
-        tile_width,
-        tile_height,
-        main
-    )
-
-    objects.draw_object(
-        lvl_1,
-        'Assets/Images/goal.png',
-        goal_x * tile_width,
-        goal_y * tile_height,
-        tile_width,
-        tile_height,
-        main
-    )
-
-    if player_x == goal_x and player_y == goal_y:
-        break
-
-lvl_width, lvl_height = 5, 3
-tile_width, tile_height = 92, 92
-player_x, player_y = 0, 2
-goal_x, goal_y = 4, 0
-
-tb, to = False, False
-blue_t, orange_t = [], []
-blue_w, orange_w = [], []
-walls = [(1, 1), (1, 2), (3, 0), (3, 1)]
-
-main = engine.Main()
-
-
-lvl_2 = main.window.new_window('Level 2', lvl_width * tile_width, lvl_height * tile_height)
-main.window.change_icon('Assets/Images/player.png')
-
 
 def movement_check():
     global player_x, player_y, tb, to, count
@@ -124,10 +34,10 @@ def movement_check():
             if (player_x, player_y) in walls:
                 player_x = prev_x
 
-            if (player_x, player_y) in blue_w and tb:
+            if (player_x, player_y) in blue_w and not tb:
                 player_x = prev_x
 
-            if (player_x, player_y) in orange_w and to:
+            if (player_x, player_y) in orange_w and not to:
                 player_x = prev_x
 
             if (player_x, player_y) in blue_t:
@@ -248,7 +158,7 @@ def draw_all(window: winobj):
     for ow in orange_w:
         objects.draw_object(
             window,
-            'Assets/Images/tw_orange.png' if not tb else 'Assets/Images/tw_orange_off.png',
+            'Assets/Images/tw_orange.png' if not to else 'Assets/Images/tw_orange_off.png',
             ow[0] * tile_width,
             ow[1] * tile_height,
             tile_width,
@@ -267,16 +177,63 @@ def draw_all(window: winobj):
     )
 
 
-while main.window.running(lvl_2):
-    main.draw.draw_rect(lvl_2, 0, 0, lvl_width * tile_width, lvl_height * tile_height, (0, 0, 0, 255))
+lvl_width, lvl_height = 3, 1
+tile_width, tile_height = 128, 128
+player_x, player_y = 0, 0
+goal_x, goal_y = 2, 0
+
+tb, to = False, False  # whether blue and orange are triggered
+blue_t, orange_t = [], []  # blue and orange triggers
+blue_w, orange_w = [], []  # blue and orange walls
+walls = []
+
+main = engine.Main()
+
+# Level 1
+
+lvl_1 = main.window.new_window('Level 1', lvl_width * tile_width, lvl_height * tile_height)
+main.window.change_icon('Assets/Images/player.png')
+
+while main.window.running(lvl_1):
+    main.draw.draw_rect(lvl_1, 0, 0, lvl_width * tile_width, lvl_height * tile_height, (0, 0, 0, 255))
+
     movement_check()
-    draw_all(lvl_2)
+    draw_all(lvl_1)
 
     if player_x == goal_x and player_y == goal_y:
         break
 
 lvl_width, lvl_height = 5, 3
 tile_width, tile_height = 92, 92
+player_x, player_y = 0, 2
+goal_x, goal_y = 4, 0
+
+tb, to = False, False
+blue_t, orange_t = [], []
+blue_w, orange_w = [], []
+walls = [(1, 1), (1, 2), (3, 0), (3, 1)]
+
+main = engine.Main()
+
+
+lvl_2 = main.window.new_window('Level 2', lvl_width * tile_width, lvl_height * tile_height)
+main.window.change_icon('Assets/Images/player.png')
+
+# Level 2
+
+while main.window.running(lvl_2):
+    main.draw.draw_rect(lvl_2, 0, 0, lvl_width * tile_width, lvl_height * tile_height, (0, 0, 0, 255))
+
+    movement_check()
+    draw_all(lvl_2)
+
+    if player_x == goal_x and player_y == goal_y:
+        break
+
+# Level 3
+
+lvl_width, lvl_height = 5, 3
+tile_width, tile_height = 96, 96
 player_x, player_y = 0, 0
 goal_x, goal_y = 4, 2
 
@@ -290,8 +247,33 @@ main.window.change_icon('Assets/Images/player.png')
 
 while main.window.running(lvl_3):
     main.draw.draw_rect(lvl_3, 0, 0, lvl_width * tile_width, lvl_height * tile_height, (0, 0, 0, 255))
+
     movement_check()
     draw_all(lvl_3)
+
+    if player_x == goal_x and player_y == goal_y:
+        break
+
+# Level 4
+
+
+lvl_width, lvl_height = 10, 6
+tile_width, tile_height = 64, 64
+player_x, player_y = 0, 0
+goal_x, goal_y = 9, 0
+
+lvl_4 = main.window.new_window('Level 4', lvl_width * tile_width, lvl_height * tile_height)
+main.window.change_icon('Assets/Images/player.png')
+tb, to = False, False
+blue_t, orange_t = [(2, 5)], [(7, 0)]
+blue_w, orange_w = [(6, 0), (6, 1), (6, 2), (7, 2), (8, 2), (9, 2)], [(8, 0), (8, 1), (9, 1)]
+walls = []
+
+while main.window.running(lvl_4):
+    main.draw.draw_rect(lvl_3, 0, 0, lvl_width * tile_width, lvl_height * tile_height, (0, 0, 0, 255))
+
+    movement_check()
+    draw_all(lvl_4)
 
     if player_x == goal_x and player_y == goal_y:
         break
